e7234d54d6041804831bef649edbede43c398a92
Van Kim Arda <Ardavankim8@gmail.com>
2026-04-27 09:36:06 +0800

    sample namo

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+include 'db.php';
+
+$result = $conn->query("SELECT * FROM students");
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Students CRUD</title>
+</head>
+
+<body>
+
+    <h2>Student List</h2>
+
+    <a href="create.php">Add New Student</a>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Course</th>
+            <th>Actions</th>
+        </tr>
+
+        <?php while($row = $result->fetch_assoc()) { ?>
+
+        <tr>
+            <td><?php echo $row['id']; ?></td>
+            <td><?php echo $row['name']; ?></td>
+            <td><?php echo $row['email']; ?></td>
+            <td><?php echo $row['course']; ?></td>
+            <td>
+                <a href="edit.php?id=<?php echo $row['id']; ?>">Edit</a>
+                <a href="delete.php?id=<?php echo $row['id']; ?>">Delete</a>
+            </td>
+        </tr>
+
+        <?php } ?>
+
+    </table>
+
+</body>
+
+</html>
